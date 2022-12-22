1b46333cd0f55113ffe70b70137685192d37f860
Michael Maher <z5362662@ad.unsw.edu.au>
2022-12-22 22:27:00 +1100

    Added all events+ started on path generation

--- generate_events.py
+++ generate_events.py
@@ -1,6 +1,10 @@
+import random
+
 # All the probabilities for each step
 
 PERSONAL_OR_CORPORATE = [0.1, 0.9]
+
+PERSONAL_ON_LOCATION = [0.2, 0.8]
 
 COROPRATE_BOOKING = [0.97, 0.01, 0.02]
 PERSONAL_BOOKING = [0.925, 0.055, 0.02]
@@ -13,6 +17,24 @@ REMINDER_OF_PHOTOS = [0.75, 0.25]
 CUSTOMER_AFTER_REMINDER = [0.25, 0.075, 0.675]
 
 NEEDS_EDITING_INITIALLY = [0.5, 0.5]
+
+NEEDS_TO_TALK_WITH_TECHNICIAN = [0.25, 0.75]
+
+NEEDS_ANOTHER_TECHNICIAN_TALK = [0.1, 0.9]
+
+GETTING_PRINTED = [0.1, 0.9]
+
+GETTING_DIGITAL = [0.95, 0.05]
+
+INVOICE_REMINDER = [0.15, 0.85]
+
+DIGITAL_OR_FEE_STRAIGHT_TO_UPDATE = [0.95, 0.05]
+
+PRINTED_STRAIGHT_TO_UPDATE = [0.5, 0.5]
+
+SEND_PRINTOUT_OR_PICKUP = [0.9, 0.1]
+
+PHOTOS_REMINDER_SENT = [0.1, 0.9]
 
 # Special names for each outcome (to make code easier to read)
 
@@ -47,21 +69,54 @@ CUSTOMER_INVOICED = 2
 EDITING_REQUIRED = 0
 EDITING_NOT_REQUIRED = 1
 
+# When a customer requires editing, whether they need to speak to a technician or not
+# ALSO IS USED FOR REPEAT STEP
+TECHNICIAN_REQUIRED = 0
+TECHNICIAN_NOT_REQUIRED = 1
+
+# Printed/Digital Photos
+
+GETTING_PRINTED = 0
+NOT_GETTING_PRINTED = 1
+
+GETTING_DIGITAL = 0
+NOT_GETTING_DIGITAL = 1
+
+# For the invoice step
+
+NEEDS_INVOICE_REMINDER = 0
+DOESNT_NEED_INVOICE_REMINDER = 1
+
+# Whether order is updated immediately or not
+
+ORDER_UPDATED = 0
+ORDER_NOT_UPDATED = 1
+
+# Whether printout is sent or photos are picked up
+
+PRINTOUT_SENT = 0
+PRINTS_TO_BE_PICKED_UP = 1
+
+# Whether reminder is sent or not (for pickup)
+
+PICKUP_REMINDER_SENT = 0
+PICKUP_SUCCESSFUL = 1
+
 # Function that generates outcome
 
-def generate_outcome(*args) -> int:
+def generate_outcome(probabilites) -> int:
     """
     Generates a certain outcome given a list of probabilities
     """
     
     # Quick sanity check to ensure probabilities add up to 1
-    if args.sum != 1:
+    if sum(probabilites) != 1:
         raise ValueError
 
     r = random.random()
 
-    for i in range(args):
-        if r < args[0:i].sum:
+    for i in range(len(probabilites)):
+        if r < sum(probabilites[0:i+1]):
             return i
     
     # If this point is reached, there is an error
--- venvtest.py
+++ venvtest.py
@@ -2,8 +2,12 @@ import simpy
 import random
 from generate_events import *
 
-NUM_STUDIOS = 4
+import csv
+
+NUM_STUDIOS = 1
 NUM_ORDERS = 100
+
+NUM_EDITORS = 2
 
 class Fotof_studio(object):
     """
@@ -27,16 +31,24 @@ class Fotof_studio(object):
 # HELPER FUNCTIONS 
 
 def get_num_photographers():
-    return 1
+    return random.randint(3,4)
 
-def get_num_editors():
-    return 1
-
+def get_studio_or_location(is_personal):
+    if not(is_personal):
+        return 1
+    else:
+        if generate_outcome(PERSONAL_ON_LOCATION) == ON_LOCATION:
+            return 1
+        else:
+             return 0
 
 
 # Simulate the use of a fotof studio for a single customer
-def use_fotof(env, fotof_studio, customer):
-    start_time = env.now
+def use_fotof(env, fotof_studio, customer, writer):
+    
+    is_personal = generate_outcome(PERSONAL_OR_CORPORATE)
+
+
 
     with fotof_studio.photographer.request() as request:
         yield request
@@ -50,31 +62,35 @@ def use_fotof(env, fotof_studio, customer):
 
 
 # Simulate the operation of the entire fotof studio for a list of orders
-def simulate_fotof_studio(env, fotof_studio, orders: list):
+def simulate_fotof_studio(env, fotof_studio, orders: list, writer):
     for customer in orders:
         yield env.timeout(1)
-        env.process(use_fotof(env, fotof_studio, customer))
+        env.process(use_fotof(env, fotof_studio, customer, writer))
 
 
 if __name__ == "__main__":
     # Create environment to be simulated
     env = simpy.Environment()
 
-    # Create the 4 different studios
+    # Create the different studios
     fotof_studios = []
     for i in range(NUM_STUDIOS):
-        fotof_studios.append(Fotof_studio(env, get_num_photographers(), get_num_editors()))
+        fotof_studios.append(Fotof_studio(env, get_num_photographers(), NUM_EDITORS))
 
     # Order ids contains the order ids for each studio
-    order_ids = [[],[],[],[]]
+    order_ids = [[] for _ in range(NUM_STUDIOS)]
 
     # Give each studio their orders and their ids
     for i in range(NUM_ORDERS):
         order_ids[random.randint(0, (NUM_STUDIOS-1))].append(i+1)
 
+    with open('eventlog.csv', 'w') as file:
+        writer = csv.writer(file)
 
-    # Simulate the 4 studios
-    for studio_num in range(NUM_STUDIOS):
-        print(f"Starting studio: {studio_num+1}\nNum orders: {len(order_ids[studio_num])}")
-        env.process(simulate_fotof_studio(env, fotof_studios[studio_num], order_ids[studio_num]))
-        env.run()
+        # Simulate the studios
+        for studio_num in range(NUM_STUDIOS):
+            print(f"Starting studio: {studio_num+1}\nNum orders: {len(order_ids[studio_num])}")
+            env.process(simulate_fotof_studio(env, fotof_studios[studio_num], order_ids[studio_num], writer))
+            env.run()
+
+        file.close()

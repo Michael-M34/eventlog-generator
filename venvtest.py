c8a70cb5827df7cf0f9ac8e6a5b796d013bd4798
Michael Maher <z5362662@ad.unsw.edu.au>
2023-01-02 11:46:05 +1100

    Path generation step complete

--- venvtest.py
+++ venvtest.py
@@ -35,20 +35,156 @@ def get_num_photographers():
 
 def get_studio_or_location(is_personal):
     if not(is_personal):
-        return 1
+        return AT_STUDIO
     else:
-        if generate_outcome(PERSONAL_ON_LOCATION) == ON_LOCATION:
-            return 1
-        else:
-             return 0
+        return generate_outcome(PERSONAL_ON_LOCATION)
 
 
 # Simulate the use of a fotof studio for a single customer
 def use_fotof(env, fotof_studio, customer, writer):
     
-    is_personal = generate_outcome(PERSONAL_OR_CORPORATE)
+    # Get info about the order 
+    is_personal = bool(generate_outcome(PERSONAL_OR_CORPORATE))
+    at_studio = bool(get_studio_or_location(is_personal))
 
+    printed = False
+    digital = False
 
+    time = 0
+
+    while True:
+
+        # Booking stage
+        while True:
+            if is_personal:
+                booking_status = generate_outcome(PERSONAL_BOOKING)
+            else:
+                booking_status = generate_outcome(COROPRATE_BOOKING)
+            
+            if booking_status == BOOKING_MADE:
+                writer.writerow([f'{customer:05d};"DETAILS ENTERED";"2020-01-01T00:00:{time:02d}"'])
+                break
+            elif booking_status == BOOKING_CANCELLED:
+                writer.writerow([f"{customer:05d}: DETAILS ENTERED"])
+                writer.writerow([f"{customer:05d}: BOOKING CANCELLED"])
+                return
+
+        # Check-in stage
+
+        writer.writerow([f"{customer:05d}: PHOTOGRAPHER CHECKED IN"])
+
+        if at_studio:
+            checkin_status = generate_outcome(STUDIO_CHECK_IN)
+        else:
+            checkin_status = generate_outcome(LOCATION_CHECK_IN)
+
+        if checkin_status == CHECK_IN_LATE:
+            writer.writerow([f"{customer:05d}: CONTACTED CUSTOMER"])
+        
+        elif checkin_status == CHECK_IN_NO_SHOW:
+            writer.writerow([f"{customer:05d}: CONTACTED CUSTOMER"])
+            
+            if generate_outcome(NO_SHOW_RESCHEDULED) == RESCHEDULED:
+                continue
+            else:
+                break
+
+        # Photos are uploaded
+        writer.writerow([f"{customer:05d}: PHOTOS UPLOADED"])
+
+        # Customer is notified of photos
+        writer.writerow([f"{customer:05d}: CUSTOMER NOTIFIED OF GALLERY"])
+
+        # Reminder step
+
+        if generate_outcome(REMINDER_OF_PHOTOS) == CUSTOMER_REMINDED:
+            writer.writerow([f"{customer:05d}: CUSTOMER REMINDED OF GALLERY"])
+            after_reminder_status = generate_outcome(CUSTOMER_AFTER_REMINDER)
+            
+            if after_reminder_status != CUSTOMER_ORDERS:
+                
+                if after_reminder_status == CUSTOMER_INVOICED:
+                    break
+
+                writer.writerow([f"{customer:05d}: INVOICE ISSUED"])
+
+        # Order is now placed   
+        writer.writerow([f"{customer:05d}: ORDER PLACED"])
+
+        # EDITING STEP
+        if generate_outcome(NEEDS_EDITING_INITIALLY) == EDITING_REQUIRED:
+            if generate_outcome(NEEDS_TO_TALK_WITH_TECHNICIAN) == TECHNICIAN_REQUIRED:
+                while True:
+                    writer.writerow([f"{customer:05d}: CUSTOMER ASKED FOR INFORMATION"])
+                    if generate_outcome(NEEDS_ANOTHER_TECHNICIAN_TALK) == TECHNICIAN_NOT_REQUIRED:
+                        break
+
+            writer.writerow([f"{customer:05d}: PHOTOS EDITED"])
+
+        # Provide info as to whether photos are printed or not
+        printed = (generate_outcome(PHOTOS_GETTING_PRINTED) == GETTING_PRINTED)
+        digital = (generate_outcome(PHOTOS_GETTING_DIGITAL) == GETTING_DIGITAL)
+
+        # Provide relevant info for printed/digital photos
+        if digital:
+            writer.writerow([f"{customer:05d}: PHOTOS UPLOADED TO DROPBOX"])
+
+        if printed:
+            writer.writerow([f"{customer:05d}: PHOTOS PRINTED"])
+
+        break
+        
+    # Invoice step
+
+    # Initial invoice issued
+    writer.writerow([f"{customer:05d}: INVOICE ISSUED"])
+
+    # Reminder step
+
+    invoice_reminder_count = 0
+
+    while generate_outcome(INVOICE_REMINDER) == NEEDS_INVOICE_REMINDER:
+        if invoice_reminder_count == 5:
+            writer.writerow([f"{customer:05d}: INVOICE REFERRED TO COLLECTIONS"])
+            return
+
+        invoice_reminder_count += 1
+        writer.writerow([f"{customer:05d}: INVOICE REMINDER SENT"])
+
+    # Actual ordering/payment step
+
+    if printed:
+        collect_payment_status = generate_outcome(PRINTED_STRAIGHT_TO_UPDATE)
+    else:
+        collect_payment_status = generate_outcome(DIGITAL_OR_FEE_STRAIGHT_TO_UPDATE)
+
+    if collect_payment_status == ORDER_NOT_UPDATED:
+        writer.writerow([f"{customer:05d}: COLLECTED PAYMENT"])
+
+    # Order updated step
+    writer.writerow([f"{customer:05d}: ORDER UPDATED"])
+
+    # Dropbox link step
+    if digital:
+        writer.writerow([f"{customer:05d}: DROPBOX LINK TO PHOTOS SENT"])
+    
+    # Printed photos received
+    if printed:
+        if collect_payment_status == ORDER_UPDATED:
+            if generate_outcome(SEND_PRINTOUT_OR_PICKUP) == PRINTOUT_SENT:
+                writer.writerow([f"{customer:05d}: PRINTOUTS SENT"])
+            else:
+                while generate_outcome(PHOTOS_REMINDER_SENT) == PICKUP_REMINDER_SENT:
+                    writer.writerow([f"{customer:05d}: PICKUP REMINDER SENT"])
+            
+    # Close order if customer ended up receiving something
+
+    if digital or printed:
+        writer.writerow([f"{customer:05d}: ORDER CLOSED"])
+
+        
+
+    
 
     with fotof_studio.photographer.request() as request:
         yield request
@@ -59,6 +195,8 @@ def use_fotof(env, fotof_studio, customer, writer):
     with fotof_studio.editor.request() as request:
         yield request
         yield env.process(fotof_studio.edit_photos(True, customer))
+
+    # Invoice stage
 
 
 # Simulate the operation of the entire fotof studio for a list of orders
@@ -86,6 +224,9 @@ if __name__ == "__main__":
 
     with open('eventlog.csv', 'w') as file:
         writer = csv.writer(file)
+
+        # Write the first row
+        writer.writerow(['CaseId;\"EventName\";\"Timestamp\"'])
 
         # Simulate the studios
         for studio_num in range(NUM_STUDIOS):
